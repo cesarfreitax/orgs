f1715970bbd99b32da332e74d6d32a0e7b1dd3d3
Cesar Freitas <cesarfreitascontato@gmail.com>
2022-09-19 20:30:57 -0300

    configurando listener de long clique nos produtos para exclusao e edicao

--- app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/ui/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -1,34 +1,56 @@
 package br.com.alura.orgs.ui.recyclerview.adapter
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.PopupMenu
 import androidx.recyclerview.widget.RecyclerView
+import br.com.alura.orgs.R
 import br.com.alura.orgs.databinding.ProdutoItemBinding
 import br.com.alura.orgs.extensions.formataParaMoedaBrasileira
 import br.com.alura.orgs.extensions.tentaCarregarImagem
 import br.com.alura.orgs.model.Produto
 
+
 class ListaProdutosAdapter(
     private val context: Context,
     produtos: List<Produto> = emptyList(),
-    var quandoClicaNoItem: (produto: Produto) -> Unit = {}
+    var quandoClicaNoItem: (produto: Produto) -> Unit = {},
+    var quandoClicaEmEditar: (produto: Produto) -> Unit = {},
+    var quandoClicaEmExcluir: (produto: Produto) -> Unit = {}
 ) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
 
     private val produtos = produtos.toMutableList()
 
     inner class ViewHolder(private val binding: ProdutoItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+        RecyclerView.ViewHolder(binding.root), PopupMenu.OnMenuItemClickListener {
 
         private lateinit var produto: Produto
 
         init {
+
             itemView.setOnClickListener {
                 if(::produto.isInitialized) {
                     quandoClicaNoItem(produto)
                 }
             }
+
+            itemView.setOnLongClickListener {
+                PopupMenu(context, itemView).apply {
+                    menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
+                    setOnMenuItemClickListener(this@ViewHolder)
+                }.show()
+                true
+            }
+
+        }
+
+
+
+        fun showPopup(v: View){
+            val popupMenu = PopupMenu(context, v)
+            val menuInflater = popupMenu.menuInflater
+            menuInflater.inflate(R.menu.menu_detalhes_produto, popupMenu.menu)
+
         }
 
         fun vincula(produto: Produto) {
@@ -52,7 +74,23 @@ class ListaProdutosAdapter(
             binding.produtoItemImagem.tentaCarregarImagem(produto.imagem)
         }
 
+        override fun onMenuItemClick(item: MenuItem?): Boolean {
+            item?.let {
+                when(it.itemId){
+                    R.id.menu_detalhes_produto_editar -> {
+                        quandoClicaEmEditar(produto)
+                    }
+                    R.id.menu_detalhes_produto_excluir -> {
+                        quandoClicaEmExcluir(produto)
+                    }
+                }
+            }
+            return true
+        }
+
     }
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
